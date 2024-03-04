ce5fc1c58044db1606835beb2e18ca70ab85ab00
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2024-03-04 11:53:01 -0300

    Add basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
     AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
