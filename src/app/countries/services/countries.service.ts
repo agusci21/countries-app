75f1dfc95b11ad0ee2e10986609c826bdba53103
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2024-03-05 10:34:17 -0300

    Refactor search methods

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,31 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiUrl = 'https://restcountries.com/v3.1';
   constructor(private httpClient: HttpClient) {}
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(url).pipe(
+      catchError(() => of([])),
+      delay(2000)
+    );
+  }
+
   searchByAlphaCode(alphaCode: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${alphaCode}`;
-    return this.httpClient.get<Country[]>(url)
-    .pipe(
-      map(countries => countries.length > 0 ? countries[0] : null),
-      catchError((_) => of(null)));
+    return this.httpClient.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError((_) => of(null))
+    );
   }
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.httpClient.get<Country[]>(url).pipe(catchError((_) => of([])));
+    return this.getCountriesRequest(url);
   }
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.httpClient.get<Country[]>(url).pipe(catchError((_) => of([])));
+    return this.getCountriesRequest(url);
   }
   searchRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`;
-    return this.httpClient.get<Country[]>(url).pipe(catchError((_) => of([])));
+    return this.getCountriesRequest(url);
   }
-
 }
